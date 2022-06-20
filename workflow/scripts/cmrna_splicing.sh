436bc79557d29da01eafe0f03d112d6495fd064a
Frances Shepherd <sheph085@umn.edu>
2022-06-20 11:16:24 -0500

    Adds back PR8-specific commands, and adds additional comments on splice junctions

--- workflow/scripts/cmrna_splicing.sh
+++ workflow/scripts/cmrna_splicing.sh
@@ -5,21 +5,21 @@ FILE=$2 #Assign the sample name provided by snakemake rule (sample)
 OUTPUT=$3 #Assign output file path provided by snakemake rule (path/to/sample_splicing_count.txt)
 
 
-## Calculate the number of reads mapped to SA/SD on the left
-#Number of reads from  both unspliced and spliced transcript
-T1_M=$(samtools depth -a -d 0 -r Cal_M:52-52 $SAMPLES)
-T1_NS=$(samtools depth -a -d 0 -r Cal_NS:56-56 $SAMPLES)	
-#Number of reads from unspliced transcript
-U1_M=$(samtools depth -a -d 0 -r Cal_M:53-53 $SAMPLES)
-U1_NS=$(samtools depth -a -d 0 -r Cal_NS:57-57 $SAMPLES)
+## Calculate the number of reads mapped to SA/SD on the left. 
+#Number of reads from  both unspliced and spliced transcript (i.e. position at the 5' side of the splice junction)
+T1_M=$(samtools depth -a -d 0 -r EF467824.1:51-51 $SAMPLES)
+T1_NS=$(samtools depth -a -d 0 -r EF467817.1:56-56 $SAMPLES)	
+#Number of reads from unspliced transcript (i.e. the position at the 3' side of the splice junction, where no spliced transcript is present).
+U1_M=$(samtools depth -a -d 0 -r EF467824.1:52-52 $SAMPLES)
+U1_NS=$(samtools depth -a -d 0 -r EF467817.1:57-57 $SAMPLES)
 
 ## Calculate the number of reads mapped to SA/SD on the right
-#Number of reads from  both unspliced and spliced transcript
-T2_M=$(samtools depth -a -d 0 -r Cal_M:741-741 $SAMPLES)
-T2_NS=$(samtools depth -a -d 0 -r Cal_NS:529-529 $SAMPLES)
-#Number of reads from unspliced transcript
-U2_M=$(samtools depth -a -d 0 -r Cal_M:740-740 $SAMPLES)
-U2_NS=$(samtools depth -a -d 0 -r Cal_NS:528-528 $SAMPLES)
+#Number of reads from  both unspliced and spliced transcript (i.e. position at the 3' side of the splice junction)
+T2_M=$(samtools depth -a -d 0 -r EF467824.1:740-740 $SAMPLES)
+T2_NS=$(samtools depth -a -d 0 -r EF467817.1:529-529 $SAMPLES)
+#Number of reads from unspliced transcript (i.e. position at the 5' side of the splice junction, where no spliced transcript is present).
+U2_M=$(samtools depth -a -d 0 -r EF467824.1:739-739 $SAMPLES)
+U2_NS=$(samtools depth -a -d 0 -r EF467817.1:528-528 $SAMPLES)
 
 ## Print out results to temporary file
 echo $T1_M	$U1_M	$T2_M	$U2_M   > $(dirname $OUTPUT)/tmp.txt
